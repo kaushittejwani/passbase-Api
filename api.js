fbdf81c6de765e6a806c5b5610034529905903e1
kaushittejwani01 <95282960+kaushittejwani@users.noreply.github.com>
2023-02-28 21:54:42 +0530

    cooment

--- api.js
+++ api.js
@@ -8,6 +8,7 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 var data="";
+//post request
 app.post('/passbase-webhooks',(req,res)=>{
      data=req.body
     res.status(200);

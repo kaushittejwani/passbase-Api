32dae06e21cb2e92a8a5c37ee25f2aa6703837c0
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2025-08-05 08:19:45 -0700

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app=express();
 const cors=require('cors')
 const bodyParser=require('body-parser');
-const port=8000;
+const port=80
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
